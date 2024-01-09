fa9789999c23acd5e592b0c5e4e01abc570a33c4
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-01-08 19:00:11 -0600

    feat: added board and files for slide puzzle

--- tcu_501/routes/index.js
+++ tcu_501/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Choose your game'});
 });
 
+router.get('/slide-puzzle', function(req, res, next) {
+  res.render('slide_puzzle', { title: 'Slide Puzzle'});
+});
+
+
 module.exports = router;
