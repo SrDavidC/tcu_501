30e59e629467b6ffa0d59bccf3d047eff33c499c
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-01-08 18:37:51 -0600

    feat: finished index as template

--- tcu_501/routes/index.js
+++ tcu_501/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Titulo' , content: "Holaa,prueba"});
+  res.render('index', { title: 'Choose your game'});
 });
 
 module.exports = router;
