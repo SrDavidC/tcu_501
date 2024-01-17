b5389d1cdbd5407d6037b636e582656716deaf06
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-01-16 21:24:43 -0600

    fix: init map characters with their descriptions

--- tcu_501/public/javascripts/slide_puzzle.js
+++ tcu_501/public/javascripts/slide_puzzle.js
@@ -1,4 +1,7 @@
 import { shuffle, playWinSound } from "./Utils.js";
+import { CharacterSP} from "./Objects/CharacterSP.js";
+// const bootstrap = require('bootstrap');
+
 let allowOnlyAdjacentMoves = false;
 
 let rows = 3;
@@ -13,13 +16,19 @@ let turns = 0;
 let path = "images/slide_puzzle/"
 // let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
-let imgCharacters =
+const imgCharacters =
   ["juan_santamaria", "carmen_lyra", "shirley_cruz", "batman", "wonder_woman", "superman"];
+const characters_map = new Map();
+
+
+
+
 
 
 
 
 window.onload = function () {
+  initCharactersMap();
   shuffle(imgOrder);
   let randomIndex = Math.floor(Math.random() * imgCharacters.length);
   let character = imgCharacters[randomIndex];
@@ -66,14 +75,19 @@ function handleWin() {
   if (isWinner) {
     console.log("Ganaste!!");
     playWinSound();
+    showWinModal();
 
   }
 }
 
 function dragEnd() {
   swapTiles();
-
+  showWinModal();
   handleWin();
+}
+
+function showWinModal() {
+  $('#WinModalCenter').modal('show');
 }
 
 function swapTiles() {
@@ -151,4 +165,55 @@ function dragDrop() {
 
 function isMobileDevice() {
   return window.innerWidth <= 768;
+}
+
+function initCharactersMap() {
+
+  // ["juan_santamaria", "carmen_lyra", "shirley_cruz", "batman", "wonder_woman", "superman"];
+
+  characters_map.set("juan_santamaria", new CharacterSP("Juan Santamaria", ["Juan Santamaria Day is a" +
+  " holiday in Costa Rica, and it is celebrated on April 11th. \n" +
+  "The holiday commemorates the Battle of Rivas, made famous by the heroic actions of the National Hero, Juan Santamaria:" +
+  " the \"Little Drummer Boy\".\n", "He is brave.", "Juan Santamaría was successful in his mission and his actions helped" +
+  " Costa Ricans win the battle, Santamaria was killed by enemy fire.\n"]))
+
+  characters_map.set("carmen_lyra", [
+    "María Isabel Carvajal (Carmen Lyra) was a writer.",
+    "She wrote and published the fairy tale collection Cuentos de mi tía Panchita and was the first female Costa Rican writer. \n"
+  , "She was an intellectual, smart, brave and visionary woman.\n",
+    "Carmen Lyra’s face is on the 20000 colones bill.\n"] )
+
+  characters_map.set("shirley_cruz", [
+    "Shirley Cruz Traña was born on August 28, 1985, in San José, Costa Rica. She is a Costa Rican soccer player.",
+    "She currently plays in the Chinese club Jiangsu Suning of the Chinese Women’s Super League.",
+    "She was the first female professional soccer player in Costa Rica and is the most successful Central American player as a professional soccer player.",
+    "She is an extraordinary athlete, strong, disciplined, confident and hard working. She is a leader."
+  ])
+
+  characters_map.set("superman", [
+    "Superman is a superhero.",
+    "He comes from a place called Krypton. His real name is Clark Kent.",
+    "He can fly, be super strong, and really fast.",
+    "Superman is a good guy who helps people",
+  ])
+
+  characters_map.set("batman", [
+    "Bruce Wayne was born in a big city called Gotham City.",
+    "He is born in a wealthy family.",
+    "When he is a child, criminals killed his parents. He promises to take revenge.",
+    "He takes a secret identity inspired in the capacities of bats and becomes Batman.",
+    "He develops special weapons to fight all criminals at Gotham city as well as mental and physical abilities.",
+    "He is very smart and can solve all types of riddles.",
+    "He can fly like bats; he has a lot of physical strength and ability to fight the villains."
+  ])
+
+  characters_map.set("wonder_woman", [
+    "Wonder Woman’s real name is Princess Diana.",
+    "She is a descendent of the mythical Amazon Women of Greece. Princess Diana is the daughter of Zeus.",
+    "Wonder Woman is immortal, this means she cannot die.",
+    "She wears bulletproof bracelets.",
+    "She also has great strength and speed.",
+    "Wonder Woman’s most special fighting tool is her Lasso of Truth. If she wraps this rope around a person and asks them questions, they tell the truth."
+  ])
+
 }
