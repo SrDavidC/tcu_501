fce46fe641815927332fa338c59cd0b402e96a26
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-01-29 17:44:20 -0600

    feat: added state and updateGrid method that updates the letters on the state to the UI

--- tcu_501/public/javascripts/wordle.js
+++ tcu_501/public/javascripts/wordle.js
@@ -1,3 +1,24 @@
+const state = {
+  grid: Array(6)
+    .fill()
+    .map(() => Array(5).fill('')),
+
+    currentRow: 0,
+    currentCol: 0,
+};
+
+function updateGrid() {
+  for (let i = 0; i < state.grid.length; i++) {
+    for (let j = 0;j < state.grid[i].length; j++) {
+      const box = document.getElementById(`box${i}${j}`);
+      if (box) {
+        box.textContent = state.grid[i][j];
+      }
+    }
+  }
+}
+
+
 function drawBox(container, row, col, letter = '') {
   const box = document.createElement('div');
   box.className = "box";
