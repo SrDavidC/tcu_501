fe30155015c5e249255eb557fe3eb822586f1c2c
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-07-25 12:54:08 -0600

    feat: added constants to url

--- tcu_501/public/javascripts/index.js
+++ tcu_501/public/javascripts/index.js
@@ -1,11 +1,17 @@
+const SLIDE_PUZZLE_ULR = "/slide-puzzle";
+const WORDLE_ULR = "/wordle";
+const INDEX = "/";
+
+
+
 document.getElementById("slide-go-btn")?.addEventListener("click", () => {
-  window.location.href = "/slide-puzzle";
+  window.location.href = SLIDE_PUZZLE_ULR ;
 });
 
 document.getElementById("wordle-go-btn")?.addEventListener("click", () => {
-  window.location.href = "/wordle";
+  window.location.href = WORDLE_ULR;
 });
 
 document.getElementById("back-btn")?.addEventListener("click", () => {
-  window.location.href = "/";
+  window.location.href = INDEX;
 });
