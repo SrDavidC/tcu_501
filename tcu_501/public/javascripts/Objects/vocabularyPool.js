3027c2e309d14755b6d3de87aca8f60e14cb5d07
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-07-25 14:24:23 -0600

    feat: now vocabulary pool is a object array

--- tcu_501/public/javascripts/Objects/vocabularyPool.js
+++ tcu_501/public/javascripts/Objects/vocabularyPool.js
@@ -1,15 +1,67 @@
 export const sixth_grade_unit_1 = [
-  "brave",
-  "funny",
-  "honor",
-  "smart",
-  "super",
-  "saved",
-  "fight",
-  "human",
-  "speed",
-  "great",
-  "crime",
-  "sense",
-  "shoot",
-]
+  {
+    word: "brave",
+    meaning: "valiente",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "funny",
+    meaning: "gracioso",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "honor",
+    meaning: "honor",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "smart",
+    meaning: "inteligente",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "super",
+    meaning: "súper",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "saved",
+    meaning: "salvado",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "fight",
+    meaning: "luchar",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "human",
+    meaning: "humano",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "speed",
+    meaning: "velocidad",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "great",
+    meaning: "genial",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "crime",
+    meaning: "crimen",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "sense",
+    meaning: "sentido",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  },
+  {
+    word: "shoot",
+    meaning: "disparar",
+    audio_path: `/sounds/words/${this.word.toLowerCase()}.wav`
+  }
+];
